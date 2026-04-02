272b7de87fe08c66589cb889f43f606173207824
maury <mauryaansh378@gmail.com>
2026-04-02 21:32:25 +0530

    done logout

--- app/src/main/java/com/example/lda/BaseActivity.kt
+++ app/src/main/java/com/example/lda/BaseActivity.kt
@@ -1,60 +1,53 @@
 package com.example.lda
 
-
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.example.lda.houseTax.LoginActivity
 import com.example.lda.houseTax.data.database.AppDatabase
 import com.example.lda.houseTax.loginDetails.SignInActivity
 import com.example.lda.houseTax.utils.PreferenceManager
 import com.example.lda.utils.SessionManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         observeLogout()
     }
 
     private fun observeLogout() {
         SessionManager.logoutLiveData.observe(this) { isLogout ->
             if (isLogout == true) {
-                performLogout()
+                // Multiple navigation avoid
+                if (!isFinishing) {
+                    SessionManager.logoutLiveData.value = false
+                    clearAppData()
+                    goToLogin()
+                }
             }
         }
     }
 
-    private fun performLogout() {
-        val preferenceManager = PreferenceManager(this)
+    private fun clearAppData() {
 
-        Log.d("TAG", "performLogout: done")
+        val preferenceManager = PreferenceManager(this)
 
         lifecycleScope.launch(Dispatchers.IO) {
             try {
                 preferenceManager.clearAll()
                 AppDatabase.getDatabase(this@BaseActivity).clearAllTables()
-
             } catch (e: Exception) {
                 e.printStackTrace()
-            }
-            withContext(Dispatchers.Main) {
-                goToLogin()
             }
         }
     }
 
     private fun goToLogin() {
         val intent = Intent(this, SignInActivity::class.java)
-        intent.flags =
-            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
         finish()
     }
